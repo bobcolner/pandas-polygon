0aac6214d654102c44b98874c98c4c1c962d523b
Bob Colner <bob@instasize.com>
2020-10-20 23:47:17 -0400

    update

--- flow_backfill.py
+++ flow_backfill.py
@@ -7,7 +7,7 @@ from polygon_backfill import get_open_market_dates, find_remaining_dates, backfi
 from polygon_s3 import get_symbol_dates
 
 
-@task(max_retries=1, retry_delay=timedelta(seconds=3))
+@task(max_retries=2, retry_delay=timedelta(seconds=2))
 def get_remaining_symbol_dates(start_date: str, end_date: str, symbols: list, tick_type: str) -> list:
     request_dates = get_open_market_dates(start_date, end_date)
     symbol_dates = []
@@ -16,17 +16,16 @@ def get_remaining_symbol_dates(start_date: str, end_date: str, symbols: list, ti
         remaining_dates = find_remaining_dates(request_dates, existing_dates)
         for date in remaining_dates:
             symbol_dates.append((symbol, date))
-
     return symbol_dates
 
 
-@task(max_retries=1, retry_delay=timedelta(seconds=3))
+@task(max_retries=2, retry_delay=timedelta(seconds=2))
 def backfill_date_task(symbol_date:tuple, tick_type:str):
-    backfill_date(
+    df = backfill_date(
         symbol=symbol_date[0],
         date=symbol_date[1],
         tick_type=tick_type,
-        result_path='/Users/bobcolner/QuantClarity/pandas-polygon/data', 
+        result_path='/Users/bobcolner/QuantClarity/pandas-polygon/data',
         upload_to_s3=True,
         save_local=True
     )
@@ -63,6 +62,7 @@ if __name__ == '__main__':
     flow_state = flow.run(
         executor=executor,
         symbols=['GOLD', 'FSM', 'SPY', 'GLD', 'GDX'],
+        # symbols=['market'],
         tick_type='trades',
         start_date='2020-01-01',
         end_date=(date.today() - timedelta(days=1)).isoformat(), # yesterday
